960921b25a114a44a017df95c147ebb1ac7f63f2
Cyruz143 <root.cyruz@gmail.com>
2016-06-01 19:42:19 +0100

    Fixing whitespace

--- ark_difficulty/config.cpp
+++ ark_difficulty/config.cpp
@@ -18,31 +18,31 @@ class CfgAILevelPresets {
 };
 
 class CfgDifficultyPresets {
-defaultPreset = "Regular";
-    class Regular {
-    description = "Why do I have to write a mod for this?!";
-  	levelAI = "AILevelLow";
-        class Options {
-            autoReport = 0;
-            cameraShake = 1;
-            commands = 0;
-            deathMessages = 0;
-            detectedMines = 0;
-            enemyTags = 0;
-            friendlyTags = 0;
-            groupIndicators = 0;
-            mapContent = 0;
-            multipleSaves = 0;
-            reducedDamage = 0;
-            scoreTable = 0;
-            staminaBar = 0;
-            stanceIndicator = 1;
-            thirdPersonView = 0;
-            visionAid = 0;
-            vonID = 0;
-            waypoints = 0;
-            weaponCrosshair = 1;
-            weaponInfo = 1;
+    defaultPreset = "Regular";
+        class Regular {
+            description = "Why do I have to write a mod for this?!";
+            levelAI = "AILevelLow";
+                class Options {
+                    autoReport = 0;
+                    cameraShake = 1;
+                    commands = 0;
+                    deathMessages = 0;
+                    detectedMines = 0;
+                    enemyTags = 0;
+                    friendlyTags = 0;
+                    groupIndicators = 0;
+                    mapContent = 0;
+                    multipleSaves = 0;
+                    reducedDamage = 0;
+                    scoreTable = 0;
+                    staminaBar = 0;
+                    stanceIndicator = 1;
+                    thirdPersonView = 0;
+                    visionAid = 0;
+                    vonID = 0;
+                    waypoints = 0;
+                    weaponCrosshair = 1;
+                    weaponInfo = 1;
+                };
         };
-    };
 };
