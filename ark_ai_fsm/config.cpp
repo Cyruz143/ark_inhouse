fda906a5aebf77be62a0d5b2324fcaee334ba0f5
Bry <cmonbryan@gmail.com>
2017-02-02 02:12:30 +0100

    FSM has debug option to output messages to diag_log.

--- ark_ai_fsm/config.cpp
+++ ark_ai_fsm/config.cpp
@@ -18,12 +18,12 @@ class Extended_PostInit_EventHandlers {
 class CfgVehicles {
 	class CAManBase;
 	class SoldierWB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\danger.fsm";
+		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
 	};
 	class SoldierEB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\danger.fsm";
+		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
 	};
 	class SoldierGB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\danger.fsm";
+		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
 	};
 };
