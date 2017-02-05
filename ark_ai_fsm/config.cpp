ebd6543ed7e0e952f845338116c2a7460eca98a6
Cyruz143 <root.cyruz@gmail.com>
2017-02-05 21:56:53 +0000

    Removing unused files

--- ark_ai_fsm/config.cpp
+++ ark_ai_fsm/config.cpp
@@ -1,29 +1,23 @@
 class CfgPatches {
-	class ark_ai_fsm {
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 1.66;
-		requiredAddons[] = { "A3_Characters_F", "CBA_MAIN" };
-		author = "ARK";
-		authorUrl = "http://www.ark-group.org";
-	};
-};
-
-class Extended_PostInit_EventHandlers {
-	class ark_ai_fsm {
-		serverInit = "[] call compile preProcessFileLineNumbers 'x\ark\addons\ark_ai_fsm\fsm_postinit.sqf';";
-	};
+    class ark_ai_fsm {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.66;
+        requiredAddons[] = {"A3_Characters_F","CBA_MAIN"};
+        author = "ARK";
+        authorUrl = "http://www.ark-group.org";
+    };
 };
 
 class CfgVehicles {
-	class CAManBase;
-	class SoldierWB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
-	};
-	class SoldierEB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
-	};
-	class SoldierGB : CAManBase {
-		fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
-	};
+    class CAManBase;
+    class SoldierWB : CAManBase {
+        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+    };
+    class SoldierEB : CAManBase {
+        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+    };
+    class SoldierGB : CAManBase {
+        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+    };
 };
