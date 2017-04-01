bece242d47901c6d0cce06b75fa9605c04ee5395
László Major <laszlo.major@outlook.com>
2017-04-01 15:28:45 +0200

    Debugging spec...

--- ark_ace_spectator/config.cpp
+++ ark_ace_spectator/config.cpp
@@ -11,7 +11,12 @@ class CfgPatches {
 
 class Extended_PreInit_EventHandlers {
     class ark_ace_spectator {
-        clientInit = "[] call compile preProcessFileLineNumbers 'x\ark\addons\ark_ace_spectator\client_spec_preinit.sqf';";
-        serverInit = "[] call compile preProcessFileLineNumbers 'x\ark\addons\ark_ace_spectator\server_spec_preinit.sqf';";
+        clientInit = "[] call compile preProcessFileLineNumbers 'x\ark\addons\ark_ace_spectator\client_preinit.sqf';";
+    };
+};
+
+class Extended_PostInit_EventHandlers {
+    class ark_ace_spectator {
+        serverInit = "[] call compile preProcessFileLineNumbers 'x\ark\addons\ark_ace_spectator\server_postinit.sqf';";
     };
 };
