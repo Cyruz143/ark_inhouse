5214ebc8563044e2a7851cac645814fe8a732fb1
Cyruz143 <root.cyruz@gmail.com>
2017-08-16 13:13:52 +0100

    Moving enhanced launchers inhouse

---
+++ ark_enhanced_launchers/config.cpp
@@ -0,0 +1,10 @@
+class CfgPatches {
+    class ark_ark_enhanced_launchers {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 1.72;
+        requiredAddons[] = { "CUP_AirVehicles_UH60", "CUP_Vehicles_ACE_compat", "CUP_AirVehicles_MH60S", "CUP_AirVehicles_AH6", "CUP_Weapons_VehicleWeapons", "A3_Weapons_F", "CUP_Weapons_Ammunition", "ace_frag", "ace_cookoff"};
+        author = "ARK, JonBons, YoursTruly";
+    };
+};
+
