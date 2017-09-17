be9b4d0b34aab8826286f3e7776d93706f936908
Cyruz143 <root.cyruz@gmail.com>
2017-09-17 20:10:27 +0100

    CUP added sounds by default, cleaning up

--- ark_enhanced_launchers/config.cpp
+++ ark_enhanced_launchers/config.cpp
@@ -2,11 +2,16 @@ class CfgPatches {
     class ark_ark_enhanced_launchers {
         units[] = {};
         weapons[] = {};
-        requiredVersion = 1.72;
-        requiredAddons[] = {"A3_Weapons_F", "CUP_Weapons_Ammunition", "ace_frag", "ace_cookoff"};
+        requiredVersion = 1.76;
+        requiredAddons[] = {"A3_Weapons_F", "CUP_Weapons_Ammunition"};
         author = "ARK, JonBons, YoursTruly";
     };
 };
+
+#define ENHANCED_AI \
+    airLock = 1; \
+    allowAgainstInfantry = 1; \
+    aiAmmoUsageFlags = "64 + 128 + 256 + 512";
 
 #define ENHANCED_LAUNCHER \
     airLock = 1; \
@@ -17,78 +22,56 @@ class CfgPatches {
 
 class CfgAmmo {
     class RocketBase;   // External Class Reference
-    class ShellBase;    // External Class Reference
-
-    class Sh_155mm_AMOS : ShellBase {
-        soundFly[] = {"x\ark\addons\ark_enhanced_launchers\shorter.wss",2,1,1000};
-    };
-    class CUP_Sh_120_HE : ShellBase {
-        soundFly[] = {"x\ark\addons\ark_enhanced_launchers\shorter.wss",2,1,1000};
-    };
-    class CUP_Sh_120_SABOT : ShellBase {
-        soundFly[] = {"x\ark\addons\ark_enhanced_launchers\shorter.wss",2,1,1000};
-    };
 
     class R_PG32V_F: RocketBase {
         ENHANCED_LAUNCHER
-        cost = 20;
     };
     class R_TBG32V_F: R_PG32V_F {
         ENHANCED_LAUNCHER
-        cost = 15;
     };
     class CUP_R_PG7V_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 25;
+        ENHANCED_AI
     };
     class CUP_R_PG7VL_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 20;
+        ENHANCED_AI
     };
     class CUP_R_PG7VR_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 25;
+        ENHANCED_AI
     };
     class CUP_R_OG7_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 15;
+        ENHANCED_AI
     };
     class CUP_R_PG9_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 20;
+        ENHANCED_AI
     };
     class CUP_R_OG9_HE: CUP_R_PG9_AT {
-        ENHANCED_LAUNCHER
-        cost = 15;
+        ENHANCED_AI
     };
     class CUP_R_SMAW_HEDP: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 20;
+        ENHANCED_AI
     };
     class CUP_R_SMAW_HEAA: CUP_R_SMAW_HEDP {
-        ENHANCED_LAUNCHER
-        cost = 100;
+        ENHANCED_AI
     };
     class CUP_R_M136_AT: RocketBase {
-        ENHANCED_LAUNCHER
+        ENHANCED_AI
     };
     class CUP_R_MEEWS_HEDP: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 25;
+        ENHANCED_AI
     };
     class CUP_R_MEEWS_HEAT: CUP_R_MEEWS_HEDP {
-        ENHANCED_LAUNCHER
-        cost = 100;
+        ENHANCED_AI
     };
     class CUP_R_RPG18_AT: RocketBase {
-        ENHANCED_LAUNCHER
-        cost = 25;
+        ENHANCED_AI
     };
     class CUP_R_SMAW_HEDP_N: RocketBase {
-        ENHANCED_LAUNCHER
+        ENHANCED_AI
     };
     class CUP_R_SMAW_HEAA_N: CUP_R_SMAW_HEDP_N {
-        ENHANCED_LAUNCHER
-        cost = 100;
+        ENHANCED_AI
+    };
+    class CUP_R_M72A6_AT: RocketBase {
+        ENHANCED_AI
     };
 };
