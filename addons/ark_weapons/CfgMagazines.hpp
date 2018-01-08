09642bdbb72909a46c1b1009d8723d5ceba73240
justwillow <justwillow@gmail.com>
2018-01-08 14:58:56 +0000

    Fixing syntax errors in RPG22

--- addons/ark_weapons/CfgMagazines.hpp
+++ addons/ark_weapons/CfgMagazines.hpp
@@ -1,20 +1,22 @@
-class CUP_RPG18_M;
-class ARK_RPG22_M : CUP_RPG18_M {
-    author = "ARK";
-    displayName = "RPG22";
-    ammo = "ARK_R_RPG22_AT";
-    initSpeed = 320; //115 unsure if breaks zero'ing to increase this
-    type = "6 * 256"; //type = "2 * 256" no clue what this does...
-    mass = 50;
-    descriptionShort = "Range: 0 - 200 m <br/>Type: HEAT <br/>Used in: RPG22";
-};
+class CfgMagazines {
+    class CUP_RPG18_M;
+    class ARK_RPG22_M: CUP_RPG18_M {
+        author = "ARK";
+        displayName = "RPG22";
+        ammo = "ARK_R_RPG22_AT";
+        initSpeed = 320; //115 unsure if breaks zero'ing to increase this
+        type = "6 * 256"; //type = "2 * 256" no clue what this does...
+        mass = 50;
+        descriptionShort = "Range: 0 - 200 m <br/>Type: HEAT <br/>Used in: RPG22";
+    };
 
-class ACE_PreloadedMissileDummy_RPG22_ARK: ARK_RPG22_M {
-    author = "ARK";
-    scope = 1;
-    scopeArsenal = 0;
-    displayName = "Preloaded Missle";
-    picture = "\CUP\Weapons\CUP_Weapons_WeaponsCore\ui\blank_icon.paa";
-    weaponPoolAvailable = 0;
-    mass = 0;
+    class ACE_PreloadedMissileDummy_RPG22_ARK: ARK_RPG22_M {
+        author = "ARK";
+        scope = 1;
+        scopeArsenal = 0;
+        displayName = "Preloaded Missle";
+        picture = "\CUP\Weapons\CUP_Weapons_WeaponsCore\ui\blank_icon.paa";
+        weaponPoolAvailable = 0;
+        mass = 0;
+    };
 };
