42fb9ac271d15258fa31c9018d1d1004b9a7178b
justwillow <justwillow@gmail.com>
2018-01-08 14:15:01 +0000

    Adding ACE compatability (backblast/disposable)

--- addons/ark_weapons/CfgAmmo.hpp
+++ addons/ark_weapons/CfgAmmo.hpp
@@ -1,46 +1,42 @@
 class CUP_R_RPG18_AT;
 class ARK_R_RPG22_AT : CUP_R_RPG18_AT {
-            hit = 450;
-            indirectHit = 25;
-            indirectHitRange = 3;
-            cost = 200;
-            initTime = 0.001;
-            thrust = 0;
-            thrustTime = 0;
-            maxSpeed = 320;
-            sideAirFriction = 0.01;
-            airfriction = 0;
-            timeToLive = 6;
-            fuseDistance = 5;
-            visibleFire = 24;
-            audibleFire = 16;
-            initSpeed = 320;
-            class CamShakeExplode
-            {
-                power = "(55 * 0.2)";
-                duration = "((round (55^0.5))*0.2 max 0.2)";
-                frequency = 20;
-                distance = "((5 + 55^0.5)*8)";
-            };
-            class CamShakeHit
-            {
-                power = 110;
-                duration = "((round (110^0.25))*0.2 max 0.2)";
-                frequency = 20;
-                distance = 1;
-            };
-            class CamShakeFire
-            {
-                power = "(20^0.25)";
-                duration = "((round (20^0.5))*0.2 max 0.2)";
-                frequency = 20;
-                distance = "((20^0.5)*8)";
-            };
-            class CamShakePlayerFire
-            {
-                power = 1;
-                duration = 0.1;
-                frequency = 20;
-                distance = 1;
-            };
+    hit = 450;
+    indirectHit = 25;
+    indirectHitRange = 3;
+    cost = 200;
+    initTime = 0.001;
+    thrust = 0;
+    thrustTime = 0;
+    maxSpeed = 320;
+    sideAirFriction = 0.01;
+    airfriction = 0;
+    timeToLive = 6;
+    fuseDistance = 5;
+    visibleFire = 24;
+    audibleFire = 16;
+    initSpeed = 320;
+    class CamShakeExplode {
+        power = "(55 * 0.2)";
+        duration = "((round (55^0.5))*0.2 max 0.2)";
+        frequency = 20;
+        distance = "((5 + 55^0.5)*8)";
+    };
+    class CamShakeHit {
+        power = 110;
+        duration = "((round (110^0.25))*0.2 max 0.2)";
+        frequency = 20;
+        distance = 1;
+    };
+    class CamShakeFire {
+        power = "(20^0.25)";
+        duration = "((round (20^0.5))*0.2 max 0.2)";
+        frequency = 20;
+        distance = "((20^0.5)*8)";
+    };
+    class CamShakePlayerFire {
+        power = 1;
+        duration = 0.1;
+        frequency = 20;
+        distance = 1;
+    };
 };
--- addons/ark_weapons/CfgWeapons.hpp
+++ addons/ark_weapons/CfgWeapons.hpp
@@ -1,10 +1,23 @@
 class CUP_launch_RPG18;
 class ARK_launch_RPG22 : CUP_launch_RPG18 {
-            author = "ARK";
-            displayName = "RPG22";
-            magazines[] = {"ARK_RPG22_M"};
-            class Library
-            {
-                libTextDesc = "The Soviet RPG-22 Netto is a one-shot disposable anti-tank rocket launcher first deployed in 1985, based on the RPG-18 rocket launcher, but firing a larger 72.5 mm fin stabilised projectile.";
-            };
+    author = "ARK";
+    displayName = "RPG22";
+    magazines[] = {"ACE_PreloadedMissileDummy_RPG22_ARK"};
+    ace_overpressure_angle = 40;
+    ace_overpressure_range = 15;
+    ace_overpressure_damage = 0.5;
+    ACE_UsedTube = "ACE_launch_RPG22_Used_ARK";
+    class Library {
+        libTextDesc = "The Soviet RPG-22 Netto is a one-shot disposable anti-tank rocket launcher first deployed in 1985, based on the RPG-18 rocket launcher, but firing a larger 72.5 mm fin stabilised projectile.";
+    };
+};
+
+class ACE_launch_RPG22_Used_ARK: ARK_launch_RPG22 {
+    scope = 1;
+    ACE_isUsedLauncher = 1;
+    author = "ARK";
+    displayName = "RPG22 (Used)";
+    descriptionShort = "Used RPG22 Tube";
+    magazines[] = {"ACE_FiredMissileDummy_RPG22_ARK"};
+    weaponPoolAvailable = 0;
 };
