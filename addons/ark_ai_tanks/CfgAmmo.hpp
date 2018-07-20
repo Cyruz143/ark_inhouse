50fb4cd7ca518812afe4bc6e8c17153e21f1372a
Cyruz143 <root.cyruz@gmail.com>
2018-07-20 13:40:37 +0100

    formatting change

--- addons/ark_ai_tanks/CfgAmmo.hpp
+++ addons/ark_ai_tanks/CfgAmmo.hpp
@@ -1,6 +1,5 @@
 class CfgAmmo {
     class Shellbase;
-    class Sh_125mm_HE;
     class Sh_120mm_HE: ShellBase {
         cost = 100;
     };
@@ -9,6 +8,7 @@ class CfgAmmo {
         cost = 100;
     };
 
+    class Sh_125mm_HE;
     class Sh_125mm_HEAT: Sh_125mm_HE {
         cost = 100;
     };
