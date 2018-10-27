5a1769143db7be8b7fa3f0253bae32371cee24a8
justwillow <just.willow@gmail.com>
2018-10-27 15:59:02 +0100

    Adding paradrop

---
+++ addons/ark_player_paradrop/CfgVehicles.hpp
@@ -0,0 +1,32 @@
+class CfgVehicles {
+    class ARK_Module;
+    class ARK_Player_Paradrop: ARK_Module {
+        scope = 2;
+        displayName = "Player Paradrop";
+        function = "ark_player_paradrop_fnc_setJumpHeight";
+        class ModuleDescription {
+            description = "Place this module to enable paradrops from planes";
+        };
+        class Arguments {
+            class Chute_Height {
+                displayName = "Set chute deploy height";
+                description = "Set height the chute will automatically deploy";
+                typeName = "NUMBER";
+                defaultValue = 200;
+            };
+        };
+    };
+
+    class Man;
+    class CAManBase: Man {
+        class ACE_SelfActions {               
+            class Start_Drop {
+                displayName = "Start Paradrop";
+                exceptions[] = {"isNotInside"};
+                condition = "call ark_player_paradrop_fnc_canDrop";
+                statement = "['ark_player_paradrop_eh_jumpController', [(vehicle player)]] call CBA_fnc_serverEvent; (vehicle player) setVariable ['ark_player_paradrop_var_jumpInProgress', true];";
+                icon = "\A3\ui_f\data\map\vehicleicons\iconParachute_ca.paa";
+            };
+        };
+    };
+};
---
+++ addons/ark_player_paradrop/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT player_paradrop
+#define COMPONENT_BEAUTIFIED Player Paradop
+
+#include "\x\ark\addons\ark_main\script_mod.hpp"
+#include "\x\ark\addons\ark_main\script_macros.hpp"
