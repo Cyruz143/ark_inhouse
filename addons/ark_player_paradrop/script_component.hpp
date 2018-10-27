ad5afff0dc99943f3a9a2800299e9acca95e66dc
justwillow <just.willow@gmail.com>
2018-10-27 16:08:09 +0100

    Fixing spelling issues and code style

--- addons/ark_player_paradrop/script_component.hpp
+++ addons/ark_player_paradrop/script_component.hpp
@@ -1,5 +1,5 @@
 #define COMPONENT player_paradrop
-#define COMPONENT_BEAUTIFIED Player Paradop
+#define COMPONENT_BEAUTIFIED Player Paradrop
 
 #include "\x\ark\addons\ark_main\script_mod.hpp"
 #include "\x\ark\addons\ark_main\script_macros.hpp"
