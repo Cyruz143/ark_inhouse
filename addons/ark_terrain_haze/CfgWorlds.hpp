6bd8a7553e41cf26b13187198b58256b38946727
Cyruz143 <root.cyruz@gmail.com>
2018-11-06 11:48:38 +0000

    Fucking tabs

--- addons/ark_terrain_haze/CfgWorlds.hpp
+++ addons/ark_terrain_haze/CfgWorlds.hpp
@@ -3,96 +3,73 @@ class CfgWorlds {
     class CAWorld: DefaultWorld {
         hazeBaseBeta0 = 2.9999998e-005;
     };
-	class Altis: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Stratis: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Tanoa: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Mountains_ACR: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Woodland_ACR: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Bootcamp_ACR: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Chernarus: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Porto: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Desert_E: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Desert_Island: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class ProvingGrounds_PMC: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Sara: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Sara_dbe1: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class SaraLite: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Shapur_BAF: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Takistan: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class utes: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Zargabad: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Abel: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Cain: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Intro: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Eden: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
-	class Noe: CAWorld
-	{
-		hazeBaseBeta0=2.9999998e-005;
-	};
+    class Altis: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Stratis: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Tanoa: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Mountains_ACR: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Woodland_ACR: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Bootcamp_ACR: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Chernarus: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Porto: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Desert_E: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Desert_Island: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class ProvingGrounds_PMC: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Sara: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Sara_dbe1: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class SaraLite: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Shapur_BAF: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Takistan: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class utes: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Zargabad: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Abel: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Cain: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Intro: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Eden: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
+    class Noe: CAWorld {
+        hazeBaseBeta0=2.9999998e-005;
+    };
 }; 
