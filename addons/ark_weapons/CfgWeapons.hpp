6ceedc2b7570c93baf7c7e6502e44f377c4e6c52
Cyruz143 <root.cyruz@gmail.com>
2018-12-09 19:01:58 +0000

    p90 rename

--- addons/ark_weapons/CfgWeapons.hpp
+++ addons/ark_weapons/CfgWeapons.hpp
@@ -12,6 +12,7 @@ class CfgWeapons {
     class arifle_SPAR_01_snd_F: arifle_SPAR_01_base_F {
         displayName = $STR_ARK_arifle_SPAR_01_snd;
     };
+    
     class arifle_SPAR_01_GL_base_F;
     class arifle_SPAR_01_GL_blk_F: arifle_SPAR_01_GL_base_F {
         displayName = $STR_ARK_arifle_SPAR_01_GL_blk;
@@ -22,6 +23,7 @@ class CfgWeapons {
     class arifle_SPAR_01_GL_snd_F: arifle_SPAR_01_GL_base_F {
         displayName = $STR_ARK_arifle_SPAR_01_GL_snd;
     };
+    
     class arifle_SPAR_02_base_F;
     class arifle_SPAR_02_blk_F: arifle_SPAR_02_base_F {
         displayName = $STR_ARK_arifle_SPAR_02_blk;
@@ -32,6 +34,7 @@ class CfgWeapons {
     class arifle_SPAR_02_snd_F: arifle_SPAR_02_base_F {
         displayName = $STR_ARK_arifle_SPAR_02_snd;
     };
+    
     class arifle_SPAR_03_base_F;
     class arifle_SPAR_03_blk_F: arifle_SPAR_03_base_F {
         displayName = $STR_ARK_arifle_SPAR_03_blk;
@@ -41,5 +44,57 @@ class CfgWeapons {
     };
     class arifle_SPAR_03_snd_F: arifle_SPAR_03_base_F {
         displayName = $STR_ARK_arifle_SPAR_03_snd;
+    };
+    
+    class SMG_03_TR_BASE;
+    class SMG_03_TR_black: SMG_03_TR_BASE {
+        displayName = $STR_ARK_PS90_TR_Black_Name;
+    };
+    class SMG_03_TR_khaki: SMG_03_TR_black {
+        displayName = $STR_ARK_PS90_TR_Khaki_Name;
+    };
+    class SMG_03_TR_camo: SMG_03_TR_black {
+        displayName = $STR_ARK_PS90_TR_Camo_Name;
+    };
+    class SMG_03_TR_hex: SMG_03_TR_BASE {
+        displayName = $STR_ARK_PS90_TR_Hex_Name;
+    };
+    class SMG_03_black: SMG_03_TR_BASE {
+        displayName = $STR_ARK_PS90_Black_Name;
+    };
+    class SMG_03_khaki: SMG_03_black {
+        displayName = $STR_ARK_PS90_Khaki_Name;
+    };
+    class SMG_03_camo: SMG_03_black {
+        displayName = $STR_ARK_PS90_Camo_Name;
+    };
+    class SMG_03_hex: SMG_03_black {
+        displayName = $STR_ARK_PS90_Hex_Name;
+    };
+    
+    class SMG_03C_BASE: SMG_03_TR_BASE {};
+    class SMG_03C_TR_black: SMG_03C_BASE {
+        displayName = $STR_ARK_P90_TR_Black_Name;
+    };
+    class SMG_03C_TR_khaki: SMG_03C_TR_black {
+        displayName = $STR_ARK_P90_TR_Khaki_Name;
+    };
+    class SMG_03C_TR_camo: SMG_03C_TR_black {
+        displayName = $STR_ARK_P90_TR_Camo_Name;
+    };
+    class SMG_03C_TR_hex: SMG_03C_TR_black {
+        displayName = $STR_ARK_P90_TR_Hex_Name;
+    };
+    class SMG_03C_black: SMG_03C_Base {
+        displayName = $STR_ARK_P90_Black_Name;
+    };
+    class SMG_03C_khaki: SMG_03C_black {
+        displayName = $STR_ARK_P90_Khaki_Name;
+    };
+    class SMG_03C_camo: SMG_03C_black {
+        displayName = $STR_ARK_P90_Camo_Name;
+    };
+    class SMG_03C_hex: SMG_03C_black {
+        displayName = $STR_ARK_P90_Hex_Name;
     };
 };
