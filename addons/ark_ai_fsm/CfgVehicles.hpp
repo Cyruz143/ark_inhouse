177614167737933d1946b09deb1b3b6c7de89bae
justwillow <justwillow@gmail.com>
2019-01-29 13:03:19 +0000

    Disabling AI FSM for testing vanilla

--- addons/ark_ai_fsm/CfgVehicles.hpp
+++ addons/ark_ai_fsm/CfgVehicles.hpp
@@ -2,14 +2,14 @@ class CfgVehicles {
     class CAManBase;
     class SoldierWB : CAManBase {
         fsmFormation = "ARK_Formation";
-        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+        //fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
     };
     class SoldierEB : CAManBase {
         fsmFormation = "ARK_Formation";
-        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+        //fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
     };
     class SoldierGB : CAManBase {
         fsmFormation = "ARK_Formation";
-        fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
+        //fsmDanger = "x\ark\addons\ark_ai_fsm\ark_danger.fsm";
     };
 };
