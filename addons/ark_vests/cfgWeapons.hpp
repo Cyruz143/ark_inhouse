45f1f5bcffd7bad3f9e98a38bcd2ad353a204f81
Cyruz143 <root.cyruz@gmail.com>
2019-07-19 20:07:25 +0100

    Add new german vests

--- addons/ark_vests/cfgWeapons.hpp
+++ addons/ark_vests/cfgWeapons.hpp
@@ -18,6 +18,9 @@ class CfgWeapons {
     class CUP_Vest_RUS_Camo_Ratnik_Base;
     class CUP_Vest_TK_Camo_Base;
     class CUP_Vest_TKI_Camo_Base;
+    class CUP_Vest_CIV_Base;
+    class CUP_Vest_CDF_6B3_Base;
+    class CUP_Vest_Ins_Camo_Base;
     class usm_vest_LBE_rm: ItemCore {
         scope = 1;
         class ItemInfo;
@@ -1130,7 +1133,7 @@ class CfgWeapons {
         scope = 1;
         class ItemInfo;
     };
-    class CUP_Vest_CIV_Base: ItemCore {
+    class CUP_V_C_Police_Holster: CUP_Vest_CIV_Base {
         scope = 1;
         class ItemInfo;
     };
@@ -1214,11 +1217,123 @@ class CfgWeapons {
         scope = 1;
         class ItemInfo;
     };
-    class CUP_Vest_CDF_6B3_Base: ItemCore {
+    class CUP_V_CDF_6B3_1_Green: CUP_Vest_CDF_6B3_Base {
         scope = 1;
         class ItemInfo;
     };
-    class CUP_Vest_Ins_Camo_Base: ItemCore {
+    class CUP_V_CDF_6B3_1_MNT: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_1_DST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_1_FST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_1_SNW: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_2_Green: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_2_MNT: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_2_DST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_2_FST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_2_SNW: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_3_Green: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_3_MNT: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_3_DST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_3_FST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_3_SNW: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_4_Green: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_4_MNT: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_4_DST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_4_FST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_4_SNW: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_5_Green: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_5_MNT: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_5_DST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_5_FST: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_6B3_5_SNW: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_CDF_CrewBelt: CUP_Vest_CDF_6B3_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_O_Ins_Carrier_Rig: CUP_Vest_Ins_Camo_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_O_Ins_Carrier_Rig_MG: CUP_Vest_Ins_Camo_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_O_Ins_Carrier_Rig_Com: CUP_Vest_Ins_Camo_Base {
+        scope = 1;
+        class ItemInfo;
+    };
+    class CUP_V_O_Ins_Carrier_Rig_Light: CUP_Vest_Ins_Camo_Base {
         scope = 1;
         class ItemInfo;
     };
@@ -2078,135 +2193,11 @@ class CfgWeapons {
         scope = 1;
         class ItemInfo;
     };
-    class CUP_V_C_Police_Holster: CUP_Vest_CIV_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_1_Green: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_1_MNT: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_1_DST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_1_FST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_1_SNW: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_2_Green: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_2_MNT: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_2_DST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_2_FST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_2_SNW: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_3_Green: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_3_MNT: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_3_DST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_3_FST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_3_SNW: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_4_Green: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_4_MNT: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_4_DST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_4_FST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_4_SNW: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_5_Green: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_5_MNT: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_5_DST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_5_FST: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_6B3_5_SNW: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_CDF_CrewBelt: CUP_Vest_CDF_6B3_Base {
-        scope = 1;
-        class ItemInfo;
-    };
     class CUP_V_CDF_OfficerBelt: CUP_V_CDF_CrewBelt {
         scope = 1;
         class ItemInfo;
     };
     class CUP_V_CDF_OfficerBelt2: CUP_V_CDF_CrewBelt {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_O_Ins_Carrier_Rig: CUP_Vest_Ins_Camo_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_O_Ins_Carrier_Rig_MG: CUP_Vest_Ins_Camo_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_O_Ins_Carrier_Rig_Com: CUP_Vest_Ins_Camo_Base {
-        scope = 1;
-        class ItemInfo;
-    };
-    class CUP_V_O_Ins_Carrier_Rig_Light: CUP_Vest_Ins_Camo_Base {
         scope = 1;
         class ItemInfo;
     };
@@ -13609,18 +13600,6 @@ class CfgWeapons {
             };
         };
     };
-    class CUP_Vest_CIV_Base_ARM: CUP_Vest_CIV_Base {
-        scope = 2;
-        class ItemInfo: ItemInfo {
-            class HitpointsProtectionInfo {
-                class Body {
-                    hitpointName = "HitBody";
-                    armor = 16;
-                    passThrough = 0.3;
-                };
-            };
-        };
-    };
     class CUP_V_C_Police_Holster_ARM: CUP_V_C_Police_Holster {
         scope = 2;
         class ItemInfo: ItemInfo {
@@ -14163,32 +14142,6 @@ class CfgWeapons {
         };
     };
     class CUP_V_CZ_vest20_ARM: CUP_V_CZ_vest20 {
-        scope = 2;
-        class ItemInfo: ItemInfo {
-            class HitpointsProtectionInfo {
-                class Chest {
-                    hitpointName = "HitChest";
-                    armor = 16;
-                    passThrough = 0.3;
-                };
-                class Diaphragm {
-                    hitpointName = "HitDiaphragm";
-                    armor = 16;
-                    passThrough = 0.3;
-                };
-                class Abdomen {
-                    hitpointName = "HitAbdomen";
-                    armor = 16;
-                    passThrough = 0.3;
-                };
-                class Body {
-                    hitpointName = "HitBody";
-                    passThrough = 0.3;
-                };
-            };
-        };
-    };
-    class CUP_Vest_CDF_6B3_Base_ARM: CUP_Vest_CDF_6B3_Base {
         scope = 2;
         class ItemInfo: ItemInfo {
             class HitpointsProtectionInfo {
@@ -14889,18 +14842,6 @@ class CfgWeapons {
         };
     };
     class CUP_V_CDF_OfficerBelt2_ARM: CUP_V_CDF_OfficerBelt2 {
-        scope = 2;
-        class ItemInfo: ItemInfo {
-            class HitpointsProtectionInfo {
-                class Body {
-                    hitpointName = "HitBody";
-                    armor = 16;
-                    passThrough = 0.3;
-                };
-            };
-        };
-    };
-    class CUP_Vest_Ins_Camo_Base_ARM: CUP_Vest_Ins_Camo_Base {
         scope = 2;
         class ItemInfo: ItemInfo {
             class HitpointsProtectionInfo {
