4ae97a6f974d79ad0fdef2ffe3f0a627f69e2fb8
justwillow <justwillow@gmail.com>
2019-10-21 18:55:09 +0100

    Adding smoke fix for 40mm

---
+++ addons/ark_smoke_fix/script_component.hpp
@@ -0,0 +1,9 @@
+#define COMPONENT ark_smoke_fix
+#define COMPONENT_BEAUTIFIED Smoke Fix
+
+#include "\x\ark\addons\ark_main\script_mod.hpp"
+#include "\x\ark\addons\ark_main\script_macros.hpp"
+
+#define SMOKE_FIX \
+    simulation = "shotSmoke"; \
+    deflectionSlowDown = 0.2;
