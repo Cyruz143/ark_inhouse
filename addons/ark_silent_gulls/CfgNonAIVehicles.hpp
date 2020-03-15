c36de453a168f53c3b4f85a1ee1a1465aeb1c296
Cyruz143 <root.cyruz@gmail.com>
2020-03-15 14:15:00 +0000

    Removing gulls for good

--- addons/ark_silent_gulls/CfgNonAIVehicles.hpp
+++
@@ -1,8 +0,0 @@
-class CfgNonAIVehicles {
-    class Bird;
-        class SeaGull : Bird {
-            model = "\A3\Weapons_F\empty.p3d";
-            moves = "";
-            singSound[] = {"A3\Sounds_F\ambient\animals\Seagul_1",0,0,0};
-        };
-};
