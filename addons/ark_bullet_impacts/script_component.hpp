d20b28abdc9bb0aa46df6038bd63c4ca536ac3f6
Cyruz143 <root.cyruz@gmail.com>
2020-07-13 20:32:20 +0100

    LFS on

---
+++ addons/ark_bullet_impacts/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT bullet_impacts
+#define COMPONENT_BEAUTIFIED Bullet Impacts
+
+#include "\x\ark\addons\ark_main\script_mod.hpp"
+#include "\x\ark\addons\ark_main\script_macros.hpp"
