6203527b28659d1d88632ae6a646ad0ef34e05f8
Cyruz143 <root.cyruz@gmail.com>
2020-07-21 10:16:26 +0100

    Revert "More littleburb tweaks"
    This reverts commit 9490bbbdd667e4a2c1f9e37d0b821b9ea8708b6e.

--- addons/ark_littlebird_tweaks/CfgVehicles.hpp
+++ addons/ark_littlebird_tweaks/CfgVehicles.hpp
@@ -3,12 +3,11 @@ class CfgVehicles {
     class CUP_AH6_BASE: Helicopter_Base_H {
         // Reduce speed a bit, at high speed handling isn't very nimble
         maxSpeed = 220; //282
-        // A2 values (I think...)
-        envelope[] = {2,3,4.25,5,6,6.4,6.4,6.6,6.6,6,4.8,3,2,1.7,1};
+
         liftForceCoef = 1.4; //1.5
         bodyFrictionCoef = 0.25; //0.4
-        cyclicAsideForceCoef = 0.5; //0.35
-        cyclicForwardForceCoef = 0.5; //0.35
+        cyclicAsideForceCoef = 0.75; //0.35
+        cyclicForwardForceCoef = 0.50; //0.35
         backRotorForceCoef = 0.8; //0.5
     };
 };
