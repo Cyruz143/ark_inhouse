ccb3e3b9cfe7be78641b92d813a649df4e4950e2
Cyruz143 <root.cyruz@gmail.com>
2020-07-25 14:45:36 +0100

    Use config method rather than CBA

---
+++ addons/ark_ai_vehicles/CfgVehicles.hpp
@@ -0,0 +1,21 @@
+class CfgVehicles {
+    class Plane;
+    class Plane_Base_F: Plane {
+        featureType = 2;
+    };
+
+    class Helicopter;
+    class Helicopter_Base_F: Helicopter {
+        featureType = 2;
+    };
+
+    class Tank;
+    class Tank_F: Tank {
+        featureType = 2;
+    };
+
+    class Car;
+    class Car_F: Car {
+        featureType = 2;
+    };
+};
--- addons/ark_ai_vehicles/config.cpp
+++ addons/ark_ai_vehicles/config.cpp
@@ -9,14 +9,18 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"ark_main"};
+        requiredAddons[] = {
+            "ark_main",
+            "CUP_Vehicles_LoadOrder"
+        };
         VERSION_CONFIG;
     };
 };
 
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        clientInit = "call compile preProcessFileLineNumbers 'x\ark\addons\ark_ai_vehicles\client_postinit.sqf';";
         serverInit = "call compile preProcessFileLineNumbers 'x\ark\addons\ark_ai_vehicles\server_postinit.sqf';";
     };
 };
+
+#include "CfgVehicles.hpp"
