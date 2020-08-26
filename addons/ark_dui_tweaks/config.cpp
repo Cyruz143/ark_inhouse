18ef0d67f57b6eb47ec6808ccff3b24da029abf8
Cyruz143 <root.cyruz@gmail.com>
2020-08-26 20:44:45 +0100

    Disable GM/IF milRadian compass stuff

---
+++ addons/ark_dui_tweaks/config.cpp
@@ -0,0 +1,24 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        author = "ARK";
+        authors[] = {"ARK"};
+        url = "http://www.ark-group.org";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "ark_main",
+            "diwako_dui_radar"
+        };
+        VERSION_CONFIG;
+    };
+};
+
+class Extended_PostInit_EventHandlers {
+    class ADDON {
+        clientInit = "call compile preProcessFileLineNumbers 'x\ark\addons\ark_dui_tweaks\client_postinit.sqf';";
+    };
+};
