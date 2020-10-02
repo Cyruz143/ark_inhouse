a01032ecbd2c775116f19065405d1474496712b0
Cyruz143 <root.cyruz@gmail.com>
2020-10-02 11:37:36 +0100

    Merge CUP light fixes in to general terrain fixes

--- addons/ark_cup_disable_lamps/script_component.hpp
+++
@@ -1,5 +0,0 @@
-#define COMPONENT cup_disable_lamps
-#define COMPONENT_BEAUTIFIED CUP Disable Lamps
-
-#include "\x\ark\addons\ark_main\script_mod.hpp"
-#include "\x\ark\addons\ark_main\script_macros.hpp"
