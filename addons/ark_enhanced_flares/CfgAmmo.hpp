514d5274a2cd64dafa336ddc8fc572a5bc63b6ec
Cyruz143 <root.cyruz@gmail.com>
2020-12-13 01:37:58 +0000

    Reducing flare intensity

--- addons/ark_enhanced_flares/CfgAmmo.hpp
+++ addons/ark_enhanced_flares/CfgAmmo.hpp
@@ -3,13 +3,13 @@ class CfgAmmo {
     class FlareBase: FlareCore {};
 
     class F_40mm_White: FlareBase {
-        intensity = 1000000; // vanilla: 10000
+        intensity = 500000; // vanilla: 10000
         timeToLive = 40; // vanilla: 25
         coefGravity = 0.25; // vanilla: undefined (would be 1)
         // Makes the ammo fall the ground slower
     };
 
     class Flare_82mm_AMOS_White: FlareCore {
-        intensity = 1500000; // vanilla: 10000
+        intensity = 150000; // vanilla: 10000
     };
 };
