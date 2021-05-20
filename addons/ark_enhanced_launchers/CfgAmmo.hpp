749e10ac2250de91dff7f2f0d0f92cb8c71ad989
Cyruz143 <root.cyruz@gmail.com>
2021-05-20 15:47:38 +0100

    Add cost to launcher ammo

--- addons/ark_enhanced_launchers/CfgAmmo.hpp
+++ addons/ark_enhanced_launchers/CfgAmmo.hpp
@@ -7,51 +7,51 @@ class CfgAmmo {
         ENHANCED_LAUNCHER
     };
     class CUP_R_PG7V_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_PG7VL_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_PG7VR_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_PG7VM_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_OG7_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_PG9_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_OG9_HE: CUP_R_PG9_AT {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_SMAW_HEDP: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_SMAW_HEAA: CUP_R_SMAW_HEDP {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_M136_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_MEEWS_HEDP: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_MEEWS_HEAT: CUP_R_MEEWS_HEDP {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_RPG18_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_SMAW_HEDP_N: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_SMAW_HEAA_N: CUP_R_SMAW_HEDP_N {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
     class CUP_R_M72A6_AT: RocketBase {
-        ENHANCED_AI
+        ENHANCED_LAUNCHER
     };
 };
--- addons/ark_enhanced_launchers/script_component.hpp
+++ addons/ark_enhanced_launchers/script_component.hpp
@@ -4,14 +4,10 @@
 #include "\x\ark\addons\ark_main\script_mod.hpp"
 #include "\x\ark\addons\ark_main\script_macros.hpp"
 
-#define ENHANCED_AI \
-    airLock = 1; \
-    allowAgainstInfantry = 1; \
-    aiAmmoUsageFlags = "64 + 128 + 256 + 512";
-
 #define ENHANCED_LAUNCHER \
     airLock = 1; \
     allowAgainstInfantry = 1; \
     aiAmmoUsageFlags = "64 + 128 + 256 + 512"; \
+    cost = 40; \
     soundFly[] = {"A3\Sounds_F\weapons\Rockets\rocket_fly_1",6,0.85,500}; \
     whistleDist = 32;
