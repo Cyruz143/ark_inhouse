9134029f597d1c36d86599e0de646344fcf9237f
Cyruz143 <root.cyruz@gmail.com>
2022-05-13 16:40:23 +0100

    Simplify and standardise vests

--- addons/ark_vests/script_component.hpp
+++ addons/ark_vests/script_component.hpp
@@ -7,9 +7,43 @@
 #define HIDDEN \
     scope = 1; \
     scopeArsenal = 0; \
-    scopeCurator = 0;
+    scopeCurator = 0; \
+    class ItemInfo;
 
-#define SHOWN \
+#define ARMOUR \
     scope = 2; \
     scopeArsenal = 2; \
-    scopeCurator = 2;
+    scopeCurator = 2; \
+    class ItemInfo: ItemInfo { \
+        class HitpointsProtectionInfo { \
+            class Neck { \
+                hitpointName = "HitNeck"; \
+                armor = 20; \
+                passThrough = 0.2; \
+            }; \
+            class Arms { \
+                hitpointName = "HitArms"; \
+                armor = 20; \
+                passThrough = 0.2; \
+            }; \
+            class Chest { \
+                hitpointName = "HitChest"; \
+                armor = 20; \
+                passThrough = 0.2; \
+            }; \
+            class Diaphragm { \
+                hitpointName = "HitDiaphragm"; \
+                armor = 20; \
+                passThrough = 0.2; \
+            }; \
+            class Abdomen { \
+                hitpointName = "HitAbdomen"; \
+                armor = 20; \
+                passThrough = 0.2; \
+            }; \
+            class Body { \
+                hitpointName = "HitBody"; \
+                passThrough = 0.2; \
+            }; \
+        }; \
+    };
