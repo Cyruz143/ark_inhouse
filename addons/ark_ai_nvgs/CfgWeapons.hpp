4726daf79253ab59a8102a3a33a8e18d98564696
Cyruz143 <root.cyruz@gmail.com>
2022-10-07 14:38:45 +0100

    Add invisible thermal goggles

--- addons/ark_ai_nvgs/CfgWeapons.hpp
+++ addons/ark_ai_nvgs/CfgWeapons.hpp
@@ -1,9 +1,9 @@
 class CfgWeapons {
     class NVGoggles;
-    class NVGoggles_AI : NVGoggles {
+    class NVGoggles_AI: NVGoggles {
         author = "ARK";
-        descriptionShort = "Night Vision Goggles - AI Only";
-        displayName = "NV Goggles (AI Only)";
+        descriptionShort = "Night Vision Goggles (Hidden)";
+        displayName = "NV Goggles (Hidden)";
         model = "\A3\Weapons_F\empty.p3d";
         modelOptics = "\A3\weapons_f\reticle\optics_night";
         picture = "\A3\Weapons_F\Data\UI\gear_nvg_CA.paa";
@@ -15,5 +15,28 @@ class CfgWeapons {
                 type = 616;
                 uniformModel = "\A3\Weapons_F\empty.p3d";
             };
+    };
+    class NVGoggles_Thermal: NVGoggles {
+        author = "ARK";
+        descriptionShort = "Thermal Vision Goggles (Hidden)";
+        displayName = "TI Goggles (Hidden)";
+        model = "\A3\Weapons_F\empty.p3d";
+        modelOptics = "\A3\weapons_f\reticle\optics_night";
+        picture = "\A3\Weapons_F\Data\UI\gear_nvg_CA.paa";
+        scope = 1;
+        thermalMode[]={0,1,2,3,4,5};
+        visionMode[]=
+        {
+            "Normal",
+            "NVG",
+            "TI"
+        };
+        class ItemInfo {
+            hmdType = 0;
+            mass = 20;
+            modelOff = "\A3\Weapons_F\empty.p3d";
+            type = 616;
+            uniformModel = "\A3\Weapons_F\empty.p3d";
+        };
     };
 };
