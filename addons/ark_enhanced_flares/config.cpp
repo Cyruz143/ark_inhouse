5d875473f398e24d889560dab2c91c3b3367eb44
Cyruz143 <root.cyruz@gmail.com>
2023-02-05 23:25:54 +0000

    Remove flare fuckery, implement in ACE

--- addons/ark_enhanced_flares/config.cpp
+++ addons/ark_enhanced_flares/config.cpp
@@ -18,10 +18,4 @@ class CfgPatches {
     };
 };
 
-class Extended_PostInit_EventHandlers {
-    class ADDON {
-        serverInit = "call compileScript ['x\ark\addons\ark_enhanced_flares\server_postinit.sqf'];";
-    };
-};
-
 #include "CfgAmmo.hpp"
