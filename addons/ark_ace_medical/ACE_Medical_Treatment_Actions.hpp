5ec35678b4a0ad86c16f800e1ca6484d69e29f21
Cyruz143 <root.cyruz@gmail.com>
2023-05-18 22:36:37 +0100

    Add some subtle audio sfx for some medical actions

--- addons/ark_ace_medical/ACE_Medical_Treatment_Actions.hpp
+++ addons/ark_ace_medical/ACE_Medical_Treatment_Actions.hpp
@@ -5,4 +5,14 @@ class ace_medical_treatment_actions {
     class PatDown: BasicBandage {
         callbackSuccess = "ark_ace_medical_fnc_putOutFire";
     };
+
+    class ApplyTourniquet: BasicBandage {
+        sounds[] = {{"x\ark\addons\ark_ace_medical\data\tourniquet.ogg",5,1,50}};
+    };
+    class Splint: BasicBandage {
+        sounds[] = {{"x\ark\addons\ark_ace_medical\data\splint.ogg",5,1,50}};
+    };
+    class Diagnose: BasicBandage {
+        sounds[] = {{"x\ark\addons\ark_ace_medical\data\diag.ogg",5,1,50}};
+    };
 };
