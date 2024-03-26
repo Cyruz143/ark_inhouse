8873093f7103863abf2df2236e91f52ece22bd4b
Cyruz143 <root.cyruz@gmail.com>
2024-03-26 13:14:26 +0000

    Remover terrain haze, force server config

--- addons/ark_terrain_haze/script_component.hpp
+++
@@ -1,9 +0,0 @@
-#define COMPONENT terrain_haze
-#define COMPONENT_BEAUTIFIED Terrain Haze
-
-#include "\x\ark\addons\ark_main\script_mod.hpp"
-#include "\x\ark\addons\ark_main\script_macros.hpp"
-
-#define REDUCED_HAZE \
-    hazeBaseBeta0 = 2.9999998e-005; \
-    HazeDensityDecay = 0.000125;
