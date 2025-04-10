418cd33eaa051f738bab022b6ee06b665f3babde
Mike-MF <TyroneMF@hotmail.com>
2025-04-10 20:35:35 +0100

    Player Screams - Update to CBA Standard

--- addons/player_screams/script_component.hpp
+++ addons/player_screams/script_component.hpp
@@ -3,3 +3,15 @@
 
 #include "\x\ark\addons\main\script_mod.hpp"
 #include "\x\ark\addons\main\script_macros.hpp"
+
+#define MACRO_PLAYER_SCREAMS \
+    "hit_snd_01", \
+    "hit_snd_02", \
+    "hit_snd_03", \
+    "hit_snd_04", \
+    "hit_snd_05", \
+    "hit_snd_06", \
+    "hit_snd_07", \
+    "hit_snd_08", \
+    "hit_snd_09", \
+    "hit_snd_10"
