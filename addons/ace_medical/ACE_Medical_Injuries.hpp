fb1b59641848169c3ea99c86043fee8a772cc375
Mike-MF <TyroneMF@hotmail.com>
2025-04-11 21:12:41 +0100

    Fix the woundhandler

--- addons/ace_medical/ACE_Medical_Injuries.hpp
+++ addons/ace_medical/ACE_Medical_Injuries.hpp
@@ -3,7 +3,7 @@ class ACE_Medical_Injuries {
         class woundHandlers;
         class falling {
             class woundHandlers: woundHandlers {
-                ADDON = QUOTE(call FUNC(handleFallDamage));
+                ADDON = QUOTE({FUNC(handleFallDamage)});
             };
         };
     };
