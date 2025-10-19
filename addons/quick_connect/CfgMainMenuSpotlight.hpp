feda5a1912ceaf6417f2a6f7fc9ba20cb8d956df
Cyruz143 <jbworrall@proton.me>
2025-10-20 00:40:41 +0100

    Add quick connect to GH

---
+++ addons/quick_connect/CfgMainMenuSpotlight.hpp
@@ -0,0 +1,62 @@
+class CfgMainMenuSpotlight {
+    class GVAR(joinServer) {
+        text = "Join ARK server";
+        textIsQuote = 0;
+        picture = QPATHTOF(resource\button.paa);
+        action = QUOTE([ARR_2('arma.ark-group.org',2320)] call (uiNamespace getVariable QQFUNC(joinServer)));
+        actionText = "Sessions are Saturdays from 7pm (UK TIME)";
+        condition = "true";
+    };
+
+    delete ApexProtocol;
+    delete BootCamp;
+    delete EastWind;
+
+    delete Orange_CampaignGerman;
+    delete Orange_Showcase_IDAP;
+    delete Orange_Showcase_LoW;
+    delete Orange_Campaign;
+
+    delete Showcase_TankDestroyers;
+
+    delete Tacops_Campaign_03;
+    delete Tacops_Campaign_02;
+    delete Tacops_Campaign_01;
+
+    delete Tanks_Campaign_01;
+
+    delete OldMan;
+
+    delete Contact_Campaign;
+
+    delete gm_campaign_01;
+
+    delete SP_FD14;
+
+    delete AoW_Showcase_AoW;
+    delete AoW_Showcase_Future;
+
+    // Reaction Forces
+    delete Scenario_TrainingDay_RF;
+    delete Scenario_FireSeason_RF;
+    delete Scenario_AirControl_RF;
+
+    // Western Sahara
+    delete Extraction_lxWS;
+    delete Showcase_Alchemist_lxWS;
+    delete Showcase_VR_lxWS;
+
+    // S.O.G. Prairie Fire
+    delete vn_showcase_macv;
+    delete vn_showcase_macv_13;
+    delete vn_showcase_pavn;
+    delete vn_showcase_pavn_13;
+    delete vn_sogba;
+
+    // Spearhead 1944
+    delete SPE_Arsenal;
+    delete SPE_Campagin_OperationCobra_04;
+    delete SPE_Coop_DerZahnarzt;
+    delete SPE_Faction_Showcase_GER;
+    delete SPE_Faction_Showcase_US;
+};
---
+++ addons/quick_connect/script_component.hpp
@@ -0,0 +1,5 @@
+#define COMPONENT quick_connect
+#define COMPONENT_BEAUTIFIED Quick Connect
+
+#include "\x\ark\addons\main\script_mod.hpp"
+#include "\x\ark\addons\main\script_macros.hpp"
