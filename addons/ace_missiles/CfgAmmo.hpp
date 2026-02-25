e4dfd6a94cfe05e440b36a910c02546c1a670321
Cyruz143 <jbworrall@proton.me>
2026-02-25 22:25:02 +0000

    Disable ACE MG on HIND ammo

---
+++ addons/ace_missiles/CfgAmmo.hpp
@@ -0,0 +1,13 @@
+class CfgAmmo {
+    class MissileBase;
+
+    class CUP_M_Shturm_9M114_AT6_Spiral_AT: MissileBase {
+        maneuvrability = 22;
+        DISABLE_ACE;
+    };
+
+    class CUP_M_Ataka_V_9M120_AT9_Spiral_2_AT: MissileBase {
+        maneuvrability = 26;
+        DISABLE_ACE;
+    };
+};
---
+++ addons/ace_missiles/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        author = "ARK";
+        authors[] = {"ARK"};
+        url = "http://www.ark-group.org";
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "ark_main",
+            "ace_compat_cup_weapons_missileguidance"
+        };
+        skipWhenMissingDependencies = 1;
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgAmmo.hpp"
